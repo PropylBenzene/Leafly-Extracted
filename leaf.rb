d107b9309d00eb01d84929b135793cb8e154b3aa
PropylBenzene <Nonas.Nonce@gmail.com>
2015-07-03 20:21:33 -0700

    Now properly appends a hash of the slug and its display name into each record in the array and properly shows the name (albeit the slug) in the .sort_by Price feature.

--- leaf.rb
+++ leaf.rb
@@ -2,57 +2,48 @@ require 'rubygems'
 require 'json'
 require 'jsonpath'
 
-#--------------This chunk deals with getting the Location Slugs--
-#Returns a long string  vvv
+locations = `curl -v -H "app_id:6682ef51" -H "app_key:55c6b0efcd2e2549ff360a5dde136a50" -X POST "http://data.leafly.com/locations" -d 'page=0&take=5&latitude=45.4278&longitude=-122.7789'`
 
-locations = `curl -v -H "app_id:6682ef51" -H "app_key:55c6b0efcd2e2549ff360a5dde136a50" -X POST "http://data.leafly.com/locations" -d 'page=0&take=20&latitude=45.4278&longitude=-122.7789'`
-
-#This parses it out, but into a HASH named 'stores' which houses an array that must be iterated through for "name".
 locations_parsed = JSON.parse(locations)
-#Need this corrected for isolating the name. $(Root Object).(Hash Name)stores[?(Filter)(@.name != "")(Inside the Current Object by the hash name "name" for anything that doesn't have "name" blank!)
+
 isolate_slugs = JsonPath.new('$.stores[?(@.name != "")]')
 slugs = isolate_slugs.on(locations)
 e = slugs.each.map { |x| x["name"]}
 f = slugs.each.map { |x| x["slug"]}
 names = f.zip(e)
 
-#-------This loop iterates through the slugs for their menu!-----
-
 @sata = ""
+@b
+@results
+@formatted_data
+@something = Array.new
 names.each do |x|
+puts x.inspect
+puts x
 @sata += `curl -v -H "app_id:6682ef51" -H "app_key:55c6b0efcd2e2549ff360a5dde136a50" -X GET "http://data.leafly.com/locations/#{x[0]}/menu"`
+puts Hash[*x.flatten]
+@sata.gsub!("\\/", "/") #\\ is for escaping the \
+@sata.gsub!("][", ",")
+@b = JSON.parse(@sata)
+@b.each { |z| z["Store"] = x[0] }
+isolate_concentrate = JsonPath.new('$.[?(@.type == "Concentrate")]')
+@formatted_data = isolate_concentrate.on(@b)
+@something += @formatted_data
+c = @something.each.map {|x| x["name"]}
+d = @something.each.map {|x| x["pricing"]}
+e = @something.each.map {|x| x["Store"]}
+@results = c.zip(d,e) #Combines the name and pricing together 
+@results.reject! { |y| y[1][0].nil? }
 sleep(5)
 end
 
-@sata.gsub!("\\/", "/") #\\ is for escaping the \
-@sata.gsub!("][", ",")
-b = JSON.parse(@sata)
-
-isolate_concentrate = JsonPath.new('$.[?(@.type == "Concentrate")]')
-formatted_data = isolate_concentrate.on(b)
-
-c = formatted_data.each.map {|x| x["name"]}
-d = formatted_data.each.map {|x| x["pricing"]}
-results = c.zip(d) #Combines the name and pricing together 
- 
-#Up until this point, it works perfectly.
-#The sorting needs to default the nil to a zero.
-
-results.reject! { |x| x[1][0].nil? }
-
-results.sort_by! do |x|
+Hash[*names.flatten]
+@results.sort_by! do |x|
 	puts x.inspect
-	x[1][0]["Price"].to_i
+	x[1][0]["Price"]
 end
-@a
 #Puts the output out.
-results.each do |x|
-puts "[%-45s]" % "#{x[0]}" + "[%-13s]" % "#{x[1][0]["Unit"]}" +  "[%+6s]" % "#{x[1][0]["Price"]}" 
+@results.each do |x|
+puts "[%-45s]" % "#{x[0]}" + "[%-30s]" % "#{x[2]}" + "[%-13s]" % "#{x[1][0]["Unit"]}" +  "[%+6s]" % "#{x[1][0]["Price"]}" 
 end
-
-
-#--How to write a file.
-output = File.open( "prices.txt","w" )
-output.write @a
-output.close
 
