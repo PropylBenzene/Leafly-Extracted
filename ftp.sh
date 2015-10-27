3f9337f1808c1a8e47a0d6c8ad053b16f84e71d8
PropylBenzene <Nonas.Nonce@gmail.com>
2015-10-27 13:01:34 -0400

    Needed.

--- ftp.sh
+++ ftp.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-HOST=ftp.extractme.esy.es  #This is the FTP servers host or IP address.
-USER=u305857047.ftp             #This is the FTP user that has access to the server.
-PASS=Contagion123X          #This is the password for the FTP user.
+HOST=f11-preview.biz.nf  #This is the FTP servers host or IP address.
+USER=1942407          #This is the FTP user that has access to the server.
+PASS=Password1!          #This is the password for the FTP user.
 
 # Call 1. Uses the ftp command with the -inv switches. 
 #-i turns off interactive prompting. 
 #-n Restrains FTP from attempting the auto-login feature. 
 #-v enables verbose and progress. 
 
-ftp -inv $HOST << EOF
+ftp -p -inv $HOST << EOF
 
 # Call 2. Here the login credentials are supplied by calling the variables.
 
@@ -19,12 +19,33 @@ user $USER $PASS
 # cd /public_html
 
 # Call4.  Here you will tell FTP to put or get the file.
-put ConcentrateSortByName.pdf
-put ConcentrateSortByPrice.pdf
-put FlowerSortByName.pdf
-put FlowerSortByPrice.pdf
+
+cd extractme.co.nf
+put ConcentratesSortByName.pdf
+put ConcentratesSortByPrice.pdf
+put FlowersSortByName.pdf
+put FlowersSortByPrice.pdf
+put ClonesSortByPrice.pdf
+put ClonesSortByName.pdf
+put SeedsSortByName.pdf
+put SeedsSortByPrice.pdf
 
 # End FTP Connection
 bye
 
 EOF
+
+#rm /home/benzene/Leaf/Leafly-Extracted/*.pdf && echo "Deleted PDF's"
+
+#Dumps latest rethinkdb database incase of a crash.
+rethinkdb dump -c 127.0.0.1:28015
+
+echo "Database Backed Up"
+
+pkill rethinkdb && echo "Shutdown"
+
+apt-get purge -y rethinkdb && echo "Removed"
+apt-get install -y rethinkdb && echo "Installed"
+
+cd /home/benzene/rethinkdb_data
+rm -r -f *
